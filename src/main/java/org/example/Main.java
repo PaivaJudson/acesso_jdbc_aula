095fbd888073831b674226ccb4a102d2509e768e
Judson Paiva <judson.paiva@hotmail.com>
2026-03-10 14:04:41 +0100

    Projecto 4 - inicio

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,30 +2,63 @@ package org.example;
 
 import org.example.db.DB;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 
 public class Main {
     static void main() {
 
         Connection connection = null;
+        PreparedStatement preparedStatement = null;
+
+        try{
+            connection = DB.getConnection();
+
+            String str =  "INSERT INTO seller(Name, Email, BirthDate, BaseSalary, DepartmentId)"
+                        + "VALUES" + "(?, ?, ?, ?, ?)";
+            preparedStatement = connection.prepareStatement(str);
+
+            preparedStatement.setString(1, "Judson Paiva");
+            preparedStatement.setString(2, "judson.paiva@isptec.co.ao");
+            preparedStatement.setDate(3, new java.sql.Date(System.currentTimeMillis()));
+            preparedStatement.setDouble(4, 9000000);
+            preparedStatement.setInt(5, 4);
+
+            int linhas = preparedStatement.executeUpdate();
+
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }finally {
+            DB.closeStatement(preparedStatement);
+            DB.closeConnection();
+        }
+    }
+
+
+
+
+
+
+
+
+
+    static void insercao() {
+        Connection connection = null;
         Statement statement = null;
         ResultSet resultSet = null;
 
-        try{
+        try {
             connection = DB.getConnection();
             statement = connection.createStatement();
             resultSet = statement.executeQuery("SELECT * FROM department");
 
-            while(resultSet.next()){
+            while (resultSet.next()) {
                 System.out.println(resultSet.getInt("Id") + ", " + resultSet.getString("Name"));
             }
         } catch (SQLException e) {
             throw new RuntimeException(e);
-        }finally {
+        } finally {
             DB.closeResultSet(resultSet);
             DB.closeStatement(statement);
             DB.closeConnection();
